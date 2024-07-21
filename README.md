069fb4b2d3010d9f087c5220e634dce925ff8b43
DINIZAINU <dini.zainu@gmail.com>
2024-07-22 06:24:26 +0700

    First init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cms-dashboard-web
+
+<p>This Project is SoftwareDevelopersToday Project for CMS Dashboard Web. Running on Spring Boot platform.</p>
---
+++ src/main/java/id/co/softwaredeveloperstoday/cms/dashboard/web/CmsDashboardWebApplication.java
@@ -0,0 +1,13 @@
+package id.co.softwaredeveloperstoday.cms.dashboard.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsDashboardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsDashboardWebApplication.class, args);
+	}
+
+}
