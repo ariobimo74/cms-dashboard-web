28bd7cd3d40223c9aaa12a8d12f6d17ca68c5341
DINIZAINU <dini.zainu@gmail.com>
2026-06-16 19:36:12 +0700

    [#71] Refactor AuthorizationRoleLevelFactory class to handle roleLevelServiceMap injected as contructor instead of manual hashmap put
    see https://github.com/ariobimo74/cms-dashboard-web/issues/71

--- src/main/java/id/co/softwaredeveloperstoday/cms/dashboard/web/factory/AuthorizationRoleLevelFactory.java
+++ src/main/java/id/co/softwaredeveloperstoday/cms/dashboard/web/factory/AuthorizationRoleLevelFactory.java
@@ -1,11 +1,7 @@
 package id.co.softwaredeveloperstoday.cms.dashboard.web.factory;
 
 import id.co.softwaredeveloperstoday.cms.dashboard.web.service.AuthorizationRoleLevelService;
-import id.co.softwaredeveloperstoday.cms.dashboard.web.service.impl.AuthorizationRoleLevelRegularAdminServiceImpl;
-import id.co.softwaredeveloperstoday.cms.dashboard.web.service.impl.AuthorizationRoleLevelSuperAdminServiceImpl;
 import id.co.softwaredeveloperstoday.cms.dashboard.web.util.enumeration.ERoleLevel;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -13,17 +9,17 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
 public class AuthorizationRoleLevelFactory {
-
-    private final List<AuthorizationRoleLevelService> levelService;
 
     private final Map<ERoleLevel, AuthorizationRoleLevelService> roleLevelServiceMap = new HashMap<>();
 
-    public AuthorizationRoleLevelService determineService(ERoleLevel roleLevel) {
+    public AuthorizationRoleLevelFactory(List<AuthorizationRoleLevelService> levelService) {
         for (AuthorizationRoleLevelService service : levelService) {
             roleLevelServiceMap.put(service.determineRoleLevel(), service);
         }
+    }
+
+    public AuthorizationRoleLevelService determineService(ERoleLevel roleLevel) {
         return roleLevelServiceMap.get(roleLevel);
     }
 
